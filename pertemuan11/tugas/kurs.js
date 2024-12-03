e7a4f535524d9aa6d40f4360bdd2a55a4cc036ff
Azhar_Fikri <azharfikri889@gmail.com>
2024-12-03 22:45:50 +0700

    Pertemuan 11, tugas : Membuat kurs_uang dan lingkaran dengan js

--- pertemuan11/tugas/kurs.js
+++ pertemuan11/tugas/kurs.js
@@ -25,29 +25,3 @@ function conversi(){
 }
 
 
-
-
-function invers(){
-    let rupiah2 =  document.getElementById("rupiah").value;
-  
-    let hasil2 = rupiah2  / 15937;
-  
-    //  let tukar2 = document.getElementById("nilaiTukar").value;
-//     let hasil2 = null;
-
-//     if(tukar2 === "UStoRp"){
-//         hasil2 = rupiah2 / 15937;
-//    }else if(tukar2 === "SGtoRp"){
-//         hasil2 = rupiah2 / 11857;
-//    }else if(tukar2 === "RtoRp"){
-//        hasil2 = rupiah2 / 3535;
-//    }else if(tukar2 === "YtoRp"){
-//        hasil2 = rupiah2 / 107;
-//    }else if(tukar2 === "EUtoRp"){
-//        hasil2 = rupiah2 / 16765;
-//    }else if(tukar2 === "RoRp"){
-//        hasil2 = rupiah2 / 4244;
-//    }
-   
-       document.getElementById("rupiah").value = hasil2;
-}
