dc8cbbb7b594e5ad4c9a8dc02cd41c4f2981cc91
Azhar_Fikri <azharfikri889@gmail.com>
2024-12-04 14:47:20 +0700

    Pertemuan 12, tugas : Membuat program prakir dengan js

---
+++ pertemuan12/script.js
@@ -0,0 +1,31 @@
+// Tulislah program untuk menghitung biaya parkir, yang dihitung berdasarkan lama parkir
+// kendaraan yaitu selisih jam masuk dengan jam keluar kendaraan. 
+// Jika parkir 2 jam pertama maka dikenakan biaya parkir 3.000 rupiah, 
+// dan per jam berikut nya kena biaya tambahan yaitu : 1.000 rupiah.
+
+function parkir(){
+    // let harga = document.getElementById("harga");
+    // harga.innerHTML = "BIAYA PARKIR 3000"
+    let jamMasuk = Number(document.getElementById("masuk").value);
+    let jamKeluar = Number(document.getElementById("keluar").value);
+
+
+    // let total =  jamMasuk + jamKeluar;
+
+    // let hasil = document.getElementById("harga").innerHTML = ": BIAYA PARKIR " + total;
+    // console.log(typeof(hasil));
+    
+    let selisihWaktu = jamKeluar - jamMasuk ;
+    let harga_1jam = 1000;
+    let harga_2jam = 3000;
+    let duaJamPertama = 2;
+    let jamHarga =  (selisihWaktu * 1000) + 1000 ;
+    let tambahnHarga = harga_1jam + harga_2jam;
+    
+
+    if(selisihWaktu <= 2){        
+        let hasil2 = document.getElementById("harga").innerHTML = ": BIAYA PARKIR " + harga_2jam;
+    }else{
+        let hasil2 = document.getElementById("harga").innerHTML = ": BIAYA PARKIR " + jamHarga;
+    }
+}
